3968032bbddc09da292b4b7cc924ac7a31e8fd25
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-05 20:52:34 -0600

    refactor: remove useCart hook and initialize cart state directly in reducer

--- guitarla_ts/src/App.tsx
+++ guitarla_ts/src/App.tsx
@@ -1,13 +1,14 @@
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 import Guitar from "./components/Guitar";
 import Header from "./components/Header"; // Importar el componente
-import { useCart } from "./hooks/useCart";
 import { cartReducer, initialState } from "./reducers/cart-reducer";
 
 function App() {
-  const {} = useCart();
-
   const [state, dispatch] = useReducer(cartReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
     <>
