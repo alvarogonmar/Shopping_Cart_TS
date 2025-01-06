250852a539206ed55945bf5e978804fa37be5cbf
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-05 20:29:13 -0600

    refactor: remove increaseQuantity prop from Header and handle quantity increase via dispatch

--- guitarla_ts/src/components/Header.tsx
+++ guitarla_ts/src/components/Header.tsx
@@ -11,7 +11,6 @@ import type { CartActions } from "../reducers/cart-reducer";
 type HeaderProps = {
   cart: CartItem[];
   dispatch: Dispatch<CartActions>;
-  increaseQuantity: (id: Guitar["id"]) => void;
   decreaseQuantity: (id: Guitar["id"]) => void;
   clearCart: () => void;
 };
@@ -19,7 +18,6 @@ type HeaderProps = {
 export default function Header({
   cart,
   dispatch,
-  increaseQuantity,
   decreaseQuantity,
   clearCart,
 }: HeaderProps) {
@@ -98,7 +96,12 @@ export default function Header({
                               <button
                                 type="button"
                                 className="btn btn-dark"
-                                onClick={() => increaseQuantity(guitar.id)}
+                                onClick={() =>
+                                  dispatch({
+                                    type: "increase-quantity",
+                                    payload: { id: guitar.id },
+                                  })
+                                }
                               >
                                 +
                               </button>
